16514230fce41944c6b08bdfc113ee9e795490e7
Vinothhk <iamvinothkumar16@gmail.com>
2025-03-31 11:41:47 +0530

    new code

--- serial.cpp
+++ serial.cpp
@@ -2,9 +2,10 @@
 #include <termios.h>
 #include <unistd.h>
 #include <iostream>
+#include <string>
 
 int main() {
-    int serial_port = open("/dev/ttyACM0", O_WRONLY);
+    int serial_port = open("/dev/ttyACM0", O_WRONLY | O_NOCTTY);
 
     if (serial_port < 0) {
         std::cerr << "Error opening serial port!" << std::endl;
@@ -18,15 +19,16 @@ int main() {
     tty.c_cflag |= (CLOCAL | CREAD);
     tcsetattr(serial_port, TCSANOW, &tty);
 
-    const char* data = "A";  // Data to send
-    while (true)
-    {
-        /* code */
-        write(serial_port, data, 1);
-        sleep(1);
+    while (true) {
+        std::string command;
+        std::cout << "Enter Command (1 1, -1 -1, 1 0, 0 1, 0 0): ";
+        std::getline(std::cin, command);
+
+        command += "\n";  // Ensure newline for Arduino
+        write(serial_port, command.c_str(), command.length()); 
+        std::cout << "Sent: " << command;
+        usleep(100000);  // Small delay to avoid overwhelming Arduino
     }
-    
-   
 
     close(serial_port);
     return 0;
